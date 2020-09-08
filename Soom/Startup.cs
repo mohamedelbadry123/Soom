bbd9b6e803cbc78e18b19bf5ec7ebe5322e9a1d9
Mohamed Elbadry <31904289+mohamedelbadry123@users.noreply.github.com>
2020-09-08 14:40:42 +0200

    test

--- Soom/Startup.cs
+++ Soom/Startup.cs
@@ -37,6 +37,7 @@ namespace Soom
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // elbadry
+        // elbadry2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSession();
